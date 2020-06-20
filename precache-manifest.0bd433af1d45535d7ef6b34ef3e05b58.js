963d0b697485c1b05dcac4779b61e0dbe6e9e0c1
kopikob <55636811+kopikob@users.noreply.github.com>
2020-06-20 22:50:48 +0200

    Updates

---
+++ precache-manifest.0bd433af1d45535d7ef6b34ef3e05b58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dabd0f5f0140f2be6bddac193e71d5ef",
+    "url": "/country-app/index.html"
+  },
+  {
+    "revision": "8827d2cf05f57cea88e8",
+    "url": "/country-app/static/css/main.9e88e605.chunk.css"
+  },
+  {
+    "revision": "e1ec156f393c5e2030c7",
+    "url": "/country-app/static/js/2.6628b3cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/country-app/static/js/2.6628b3cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8827d2cf05f57cea88e8",
+    "url": "/country-app/static/js/main.b34cdd85.chunk.js"
+  },
+  {
+    "revision": "7768c05af8b87a749826",
+    "url": "/country-app/static/js/runtime-main.5b717548.js"
+  },
+  {
+    "revision": "cce434e704f79783f54d470b2f420b85",
+    "url": "/country-app/static/media/map.cce434e7.jpg"
+  }
+]);
