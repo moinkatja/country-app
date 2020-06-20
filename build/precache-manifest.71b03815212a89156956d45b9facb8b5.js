6f152478fe487bb1be65bf4a9355446af7aac5dd
Katja <55636811+moinkatja@users.noreply.github.com>
2020-06-20 22:38:49 +0200

    Add files via upload

---
+++ build/precache-manifest.71b03815212a89156956d45b9facb8b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef27feb791cbedf6a76ddffb486a63f",
+    "url": "/country-app/index.html"
+  },
+  {
+    "revision": "85506d5154b941930590",
+    "url": "/country-app/static/css/main.9e88e605.chunk.css"
+  },
+  {
+    "revision": "6c1520f316da92f6417a",
+    "url": "/country-app/static/js/2.03e96280.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/country-app/static/js/2.03e96280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85506d5154b941930590",
+    "url": "/country-app/static/js/main.4d966576.chunk.js"
+  },
+  {
+    "revision": "edcfe826d9d90fdf49fd",
+    "url": "/country-app/static/js/runtime-main.7623f708.js"
+  },
+  {
+    "revision": "cce434e704f79783f54d470b2f420b85",
+    "url": "/country-app/static/media/map.cce434e7.jpg"
+  }
+]);
